344ee7b3695bfbd3d87f08a7246eabbcaf111ca4
ioe <ioeory@gmail.com>
2021-05-28 17:10:20 +0800

    modify ping*.sh result output

--- ping-cnet.sh
+++ ping-cnet.sh
@@ -3,8 +3,8 @@
 for x in {1..255} 
 do
 {
-   ping 168.168.70.$x  -c 1 >/dev/null 2>&1 
-   [ $? -eq 0 ] && echo 168.168.70.$x live >>/tmp/alive.txt  || echo 168.168.70.$x dead &
+   ping 192.168.1.$x  -c 1 >/dev/null 2>&1 
+   [ $? -eq 0 ] && echo 192.168.1$x >>/tmp/alive.txt  || echo 192.168.1.$x dead &
 }&   
 done
 wait
--- ping.sh
+++ ping.sh
@@ -5,8 +5,8 @@ do
 {
   for y in {1..255}
      do {
-         ping 168.168.$x.$y  -c 1 >/dev/null 2>&1 
-         [ $? -eq 0 ] && echo 168.168.$x.$y live >>/tmp/alive.txt  || echo 168.168.$x.$y dead &
+         ping 192.168.$x.$y  -c 1 >/dev/null 2>&1 
+         [ $? -eq 0 ] && echo 192.168.$x.$y >>/tmp/alive.txt  || echo 192.168.$x.$y dead &
          }& 
          sleep 1
      done
