d65e5cdd22bac2280ab9d2f00dfbdb2db8eb3624
ioe <ioeory@gmail.com>
2021-06-15 11:11:07 +0800

    define IP variable

--- ping.sh
+++ ping.sh
@@ -4,9 +4,11 @@ for x in {1..255}
 do
 {
   for y in {1..255}
+
      do {
-         ping 192.168.$x.$y  -c 1 >/dev/null 2>&1 
-         [ $? -eq 0 ] && echo 192.168.$x.$y >>/tmp/alive.txt  || echo 192.168.$x.$y dead &
+         IP=168.168.$x.$y
+         ping $IP  -c 1 -W 1 >/dev/null 2>&1
+         [ $? -eq 0 ] && echo $IP >>/tmp/alive.txt  || echo $IP dead &
          }& 
          sleep 1
      done
